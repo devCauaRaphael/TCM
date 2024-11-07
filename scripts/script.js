dd4de71f0a97b546c5fe5d5ba04558c0cccf3e9d
Cauã Raphael <dev.cauaraphael@gmail.com>
2024-11-07 18:25:07 -0300

    desenvolvimento dos metodos do carrossel realizado

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,25 @@ const menu = document.getElementById("menu");
 menuToggle.addEventListener('click',()=>{
     menu.classList.toggle('open');
 }); 
+/*metodo do carrossel*/
+var correrSlide = 0;
+const slides = document.querySelectorAll('.slide');
+document.querySelector('.next').addEventListener('click', () => {
+ NextSlide();
+});
+document.querySelector('.prev').addEventListener('click', () => {
+    PrevSlide();
+ });
+ function NextSlide() {
+    correrSlide = (correrSlide + 1) % slides.length;
+    AlterarPosicaoSlide();
+ }
+ function PrevSlide() {
+    correrSlide = (correrSlide - 1) % slides.length;
+    AlterarPosicaoSlide();
+ }
+ function AlterarPosicaoSlide() {
+     const slidesContainer = document.querySelector('.slides');
+     slidesContainer.style.transform = `translateX(-${correrSlide * 100}%)`;
+ }
+ 
