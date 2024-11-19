c41f52e3760b605d1ef3b50e969b1b32d6939258
Cauã Raphael <dev.cauaraphael@gmail.com>
2024-11-19 17:05:50 -0300

    inicio do desenvolvimento do windows forms, primeira estrutura visual

---
+++ backEnd/BackEndTcm/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BackEndTcm
+{
+    public partial class Form1 : Form
+    {
+        String caminho = @"C:\Users\cauaz\OneDrive\Área de Trabalho\TCM\backEnd\BackEndTcm\database";
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
