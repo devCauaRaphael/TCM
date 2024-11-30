a46f43c44d80e9ebadc6c49cd7283864062df8d3
Cauã Raphael <dev.cauaraphael@gmail.com>
2024-11-29 21:04:24 -0300

    back-end desenvolvido

--- backEnd/BackEndTcm/frmAtualizar.cs
+++ backEnd/BackEndTcm/frmAtualizar.cs
@@ -29,7 +29,6 @@ namespace BackEndTcm
 
             // O caminho do arquivo de cadastro
 
-            string registroAntigo = caminho; // O registro que você quer atualizar
             string usuario = txtUsuario.Text; // O nome de usuário fornecido
             string novoNome = txtNome.Text; // Novo nome fornecido
             string novoEmail = txtEmail.Text; // Novo email fornecido
@@ -37,10 +36,10 @@ namespace BackEndTcm
             string novoTelefone = mskTelefone.Text; // Novo telefone fornecido
 
             if (string.IsNullOrWhiteSpace(usuario) ||
-     string.IsNullOrWhiteSpace(novoNome) ||
-     string.IsNullOrWhiteSpace(novoEmail) ||
-     string.IsNullOrWhiteSpace(novaSenha) ||
-     string.IsNullOrWhiteSpace(novoTelefone))
+                string.IsNullOrWhiteSpace(novoNome) ||
+                string.IsNullOrWhiteSpace(novoEmail) ||
+                string.IsNullOrWhiteSpace(novaSenha) ||
+                string.IsNullOrWhiteSpace(novoTelefone))
             {
                 MessageBox.Show("Por favor, preencha todas as informações antes de atualizar.", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
@@ -50,9 +49,9 @@ namespace BackEndTcm
                 {
                     // Lê todas as linhas do arquivo
                     var linhas = File.ReadAllLines(caminho);
-
                     bool registroAtualizado = false;
 
+                    // Itera pelas linhas para encontrar o usuário
                     for (int i = 0; i < linhas.Length; i++)
                     {
                         string linha = linhas[i];
@@ -60,6 +59,7 @@ namespace BackEndTcm
                         // Verifica se o nome de usuário está presente na linha
                         if (linha.Contains(usuario))
                         {
+                            // Atualiza o registro com as novas informações
                             linhas[i] = $"{usuario} - {novoNome} - {novoEmail} - {novaSenha} - {novoTelefone}";
 
                             // Marca que o registro foi atualizado
@@ -71,12 +71,13 @@ namespace BackEndTcm
                     // Se o registro foi encontrado e atualizado
                     if (registroAtualizado)
                     {
+                        // Sobrescreve as linhas no arquivo
                         File.WriteAllLines(caminho, linhas);
                         MessageBox.Show("Cadastro atualizado com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
                     }
                     else
                     {
-                        // Caso não tenha encontrado o usuário
+                        // Caso o usuário não tenha sido encontrado
                         MessageBox.Show("Usuário não encontrado para atualização.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
                     }
                 }
@@ -90,6 +91,7 @@ namespace BackEndTcm
             {
                 MessageBox.Show("Arquivo de cadastro não encontrado!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+
 
 
         }
--- backEnd/BackEndTcm/frmFutebol.Designer.cs
+++ backEnd/BackEndTcm/frmFutebol.Designer.cs
@@ -36,14 +36,13 @@
             this.txtIdade = new System.Windows.Forms.TextBox();
             this.btnClassificar = new System.Windows.Forms.Button();
             this.txtResultado = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(201, 9);
+            this.label1.Location = new System.Drawing.Point(346, 133);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(372, 29);
             this.label1.TabIndex = 0;
@@ -52,7 +51,7 @@
             // btnVoltar
             // 
             this.btnVoltar.ForeColor = System.Drawing.Color.Red;
-            this.btnVoltar.Location = new System.Drawing.Point(429, 384);
+            this.btnVoltar.Location = new System.Drawing.Point(597, 453);
             this.btnVoltar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnVoltar.Name = "btnVoltar";
             this.btnVoltar.Size = new System.Drawing.Size(83, 30);
@@ -65,7 +64,7 @@
             // 
             this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnLimpar.ForeColor = System.Drawing.Color.Black;
-            this.btnLimpar.Location = new System.Drawing.Point(269, 384);
+            this.btnLimpar.Location = new System.Drawing.Point(437, 453);
             this.btnLimpar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnLimpar.Name = "btnLimpar";
             this.btnLimpar.Size = new System.Drawing.Size(83, 30);
@@ -78,7 +77,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(178, 152);
+            this.label2.Location = new System.Drawing.Point(346, 221);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(141, 25);
             this.label2.TabIndex = 5;
@@ -86,7 +85,7 @@
             // 
             // txtIdade
             // 
-            this.txtIdade.Location = new System.Drawing.Point(335, 156);
+            this.txtIdade.Location = new System.Drawing.Point(503, 225);
             this.txtIdade.Name = "txtIdade";
             this.txtIdade.Size = new System.Drawing.Size(125, 22);
             this.txtIdade.TabIndex = 0;
@@ -95,7 +94,7 @@
             // btnClassificar
             // 
             this.btnClassificar.ForeColor = System.Drawing.Color.Black;
-            this.btnClassificar.Location = new System.Drawing.Point(335, 194);
+            this.btnClassificar.Location = new System.Drawing.Point(503, 263);
             this.btnClassificar.Name = "btnClassificar";
             this.btnClassificar.Size = new System.Drawing.Size(111, 23);
             this.btnClassificar.TabIndex = 1;
@@ -105,30 +104,18 @@
             // 
             // txtResultado
             // 
-            this.txtResultado.Location = new System.Drawing.Point(335, 236);
+            this.txtResultado.Location = new System.Drawing.Point(474, 305);
             this.txtResultado.Name = "txtResultado";
-            this.txtResultado.Size = new System.Drawing.Size(125, 22);
+            this.txtResultado.Size = new System.Drawing.Size(177, 22);
             this.txtResultado.TabIndex = 2;
             this.txtResultado.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(228, 236);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(101, 25);
-            this.label3.TabIndex = 9;
-            this.label3.Text = "categoria";
             // 
             // frmFutebol
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label3);
+            this.ClientSize = new System.Drawing.Size(1069, 564);
             this.Controls.Add(this.txtResultado);
             this.Controls.Add(this.btnClassificar);
             this.Controls.Add(this.txtIdade);
@@ -140,6 +127,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "frmFutebol";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Descubra a Categoria";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -155,6 +143,5 @@
         private System.Windows.Forms.TextBox txtIdade;
         private System.Windows.Forms.Button btnClassificar;
         private System.Windows.Forms.TextBox txtResultado;
-        private System.Windows.Forms.Label label3;
     }
 }
